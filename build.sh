d2155d36cbb33f5357c559ec26086d552f8f2a5b
Dmitry Smagin <exmortis@yandex.ru>
2016-10-04 18:58:02 +0300

    Fix compilation on linux (-lm option)

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 CFLAGS="-std=c99 -g -Wall -Wextra -Wno-unused-parameter -Wno-unused-function `sdl-config --cflags`"
+LDFLAGS="`sdl-config --libs` -lm"
 
 gcc	src/a2t.c src/depack.c src/sixpack.c \
 	src/ymf262.c \
-	-o a2t $CFLAGS `sdl-config --libs`
+	-o a2t $CFLAGS $LDFLAGS
