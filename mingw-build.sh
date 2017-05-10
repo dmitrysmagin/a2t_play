9896c746f82e3b3d0d4802b855ba47eda1141a94
Dmitry Smagin <exmortis@yandex.ru>
2017-05-10 10:47:27 +0300

    Add build script for mingw

---
+++ mingw-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CFLAGS="-std=c99 -g -Wall -Wextra -Wno-unused-parameter -Wno-unused-function `sdl-config --cflags`"
+LDFLAGS="-lSDL -mconsole"
+
+gcc	src/a2t.c src/depack.c src/sixpack.c \
+	src/ymf262.c \
+	-o a2t $CFLAGS $LDFLAGS
