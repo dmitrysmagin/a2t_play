412fff4434e455e41c7c0dc1ee0d75a36ae7ad96
Dmitry Smagin <exmortis@yandex.ru>
2017-05-10 12:17:27 +0300

    Fix wrong struct padding for MINGW

--- mingw-build.sh
+++ mingw-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CFLAGS="-std=c99 -g -Wall -Wextra -Wno-unused-parameter -Wno-unused-function `sdl-config --cflags`"
+CFLAGS+=" -mno-ms-bitfields"
 LDFLAGS="-lSDL -mconsole"
 
 gcc	src/a2t.c src/depack.c src/sixpack.c \
