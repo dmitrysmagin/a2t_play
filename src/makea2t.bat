e6f88979967f528d4eb8f2604f1e305db2af773e
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2020-01-02 21:12:40 +0300

    Remove pascal sources and old readme, they are not needed anymore, :)

--- src/makea2t.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Building A2T_Play
-echo Make sure you have FPC in your path
-echo.
-echo Press CTRL-C to abort, any key to compile
-pause
-ppc386 a2t_play.pas -B -Sd -XXs -OGr3 
-pause
