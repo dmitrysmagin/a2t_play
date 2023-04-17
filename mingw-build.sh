a1ba63d9872239afb30325d9b489ff393d67faaa
Dmitry Smagin <dmitry.s.smagin@gmail.com>
2023-04-17 10:13:40 +0300

    Drop -mno-ms-bitfields option for mingw

--- mingw-build.sh
+++ mingw-build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 CFLAGS="-std=c99 -g -Wall -Wextra -Wno-unused-parameter -Wno-unused-function `sdl-config --cflags`"
-CFLAGS+=" -mno-ms-bitfields"
 LDFLAGS="-lSDL -mconsole"
 
 gcc	src/sdl.c src/a2t.c src/depack.c src/sixpack.c src/unlzh.c src/opl3.c \
